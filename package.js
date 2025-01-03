4296e4cac0dc4f6a4bbacf7afad22790d7474238
Kelogno <San20119@outlook.com>
2025-01-02 22:54:32 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "auth-storage-app",
+  "version": "1.0.0",
+  "description": "Aplicación de autenticación y almacenamiento de mensajes",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  },
+  "license": "MIT"
+}
